1d71f2a1557d603ff724be636768aacc1f38c53a
seyfu-t <git@seyfu-t.me>
2026-09-15 11:40:34 +0200

    chore: cleanup popups code from dupes, finally get firefox extensions to behave correctly

--- roles/hyprland/files/popups.lua
+++ roles/hyprland/files/popups.lua
@@ -9,6 +9,19 @@ local function popup(match)
     })
 end
 
+-- Generic centered popups
+popup({ class = [[org\.freedesktop\.impl\.portal\.desktop\.kde|xdg-desktop-portal-gtk]] })
+popup({ class = [[io\.missioncenter\.MissionCenter]] })
+popup({ class = "signal" })
+popup({ class = "nm-connection-editor" })
+popup({ class = [[org\.pulseaudio\.pavucontrol]] })
+popup({ class = [[org\.kde\.ark]] })
+popup({ class = [[org\.kde\.okular]] })
+
+popup({ class = [[firefox|app\.zen_browser\.zen]], title = "Library", })
+popup({ initial_class = [[org\.mozilla\.Thunderbird|eu\.betterbird\.Betterbird]], initial_title = "Write:.*", })
+popup({ title = "Steam Settings", })
+
 -- Picture-in-Picture
 hl.window_rule({
     match = {
@@ -20,48 +33,13 @@ hl.window_rule({
     size = popup_size,
 })
 
--- Generic centered popups
-popup({ class = [[org\.freedesktop\.impl\.portal\.desktop\.kde|xdg-desktop-portal-gtk]] })
-popup({ class = [[io\.missioncenter\.MissionCenter]] })
-popup({ class = "signal" })
-popup({ class = "nm-connection-editor" })
-popup({ class = [[org\.pulseaudio\.pavucontrol]] })
-popup({ class = [[org\.kde\.ark]] })
-popup({ class = [[org\.kde\.okular]] })
-
--- Steam popups
+-- Steam friends list should stay small
 hl.window_rule({
     match = {
         title = "Friends List",
     },
     float = true,
     move = { 1616, 380 },
-})
-
-hl.window_rule({
-    match = {
-        title = "Steam Settings",
-    },
-    float = true,
-    center = true,
-})
-
--- Browser library popup
-hl.window_rule({
-    match = {
-        class = [[firefox|app\.zen_browser\.zen]],
-        title = "Library",
-    },
-    float = true,
-})
-
--- Mail compose window
-hl.window_rule({
-    match = {
-        initial_class = [[org\.mozilla\.Thunderbird|eu\.betterbird\.Betterbird]],
-        initial_title = "Write:.*",
-    },
-    float = true,
 })
 
 -- Special case for IntelliJ IDEA
@@ -72,3 +50,68 @@ hl.window_rule({
     },
     no_initial_focus = true,
 })
+
+
+-- Annoying Browser Extension bullshittery
+
+local function position_popup(w)
+    hl.dispatch(hl.dsp.window.resize({
+        x = popup_size[1],
+        y = popup_size[2],
+        window = w,
+    }))
+
+    hl.dispatch(hl.dsp.window.center({
+        window = w,
+    }))
+end
+
+hl.window_rule({
+    match = {
+        initial_class = "app\\.zen_browser\\.zen",
+        initial_title = "Zen Browser",
+    },
+    suppress_event = "maximize fullscreen",
+})
+
+hl.on("window.open", function(w)
+    if w.class ~= "app.zen_browser.zen" then return end
+    if w.initial_title ~= "Zen Browser" then return end
+
+    -- Don't affect the first/main Zen window.
+    local zen_windows = hl.get_windows({ class = "app.zen_browser.zen" })
+    if #zen_windows <= 1 then return end
+
+    -- Float immediately, before we know what this window actually is.
+    hl.dispatch(hl.dsp.window.float({
+        action = "set",
+        window = w,
+    }))
+    position_popup(w)
+
+    local sub
+    sub = hl.on("window.title", function(tw)
+        if tw.address ~= w.address then return end
+
+        -- Ignore Firefox/Zen's intermediate titles.
+        if tw.title == ""
+            or tw.title == "Zen Browser"
+            or tw.title == "about:blank" then
+            return
+        end
+
+        sub:remove()
+
+        -- Keep extension windows floating.
+        if tw.title:sub(1, #"Extension:") == "Extension:" then
+            position_popup(tw)
+            return
+        end
+
+        -- It turned out to be a normal Zen window.
+        hl.dispatch(hl.dsp.window.float({
+            action = "unset",
+            window = tw,
+        }))
+    end)
+end)
